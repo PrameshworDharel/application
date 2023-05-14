182f7e37a43409197c347e1a6e1b0e1251edbe44
Prameshwor <dharelprameshwor@gmail.com>
2023-05-14 17:35:35 +0545

    react router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from './components/Home'
+import Blog from './components/Blogs'
+import About from './components/About'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className="navbar">
+     
+      <ul className="nav-lists">
+        <li>
+          <Link to="/" >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/about" >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/blogs" >Blog
+          </Link>
+        </li>
+      </ul>
+    </nav>
+     <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/about" element={<About />} />
+     <Route path="/blogs" element={<Blog/>} />
+   </Routes>
+</>
   );
 }
 
